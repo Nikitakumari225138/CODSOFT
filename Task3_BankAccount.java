faad63111c6c470d2027410c1bd7e5baa3f4d1d3
Nikitakumari225138 <161463737+Nikitakumari225138@users.noreply.github.com>
2024-03-02 21:45:24 +0530

    Create Task3_BankAccount.java

---
+++ Task3_BankAccount.java
@@ -0,0 +1,25 @@
+class BankAccount {
+	private double balance;
+
+	public BankAccount(double initialBalance) {
+		balance = initialBalance;
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+
+	public void deposit(double amount) {
+		balance += amount;
+	}
+
+	public boolean withdraw(double amount) {
+		if (balance >= amount) {
+			balance -= amount;
+			return true;
+		} else {
+			System.out.println("Insufficient funds.");
+			return false;
+		}
+	}
+}
